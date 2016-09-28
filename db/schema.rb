4dd4c1d19c366165140cc9d27cb26759f924d42c
Jo Squire <mjsquire@digis.net>
2016-09-28 13:22:54 -0600

    style lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926203005) do
+ActiveRecord::Schema.define(version: 20160927160345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,10 +71,8 @@ ActiveRecord::Schema.define(version: 20160926203005) do
   create_table "pantries", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
-    t.integer  "ingredient_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["ingredient_id"], name: "index_pantries_on_ingredient_id", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_pantries_on_user_id", using: :btree
   end
 
