959bdbbae07179998f4a6d18c6f64986def5d39c
Lonnie Horlacher <horlacher.lonnie@gmail.com>
2016-10-04 09:27:54 -0600

    updated search feature

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -21,27 +21,82 @@ class Recipe < ApplicationRecord
 
 	class << self
 
-		def decipher_search_params(search_type, search_query)
+		def distribute_params(search_type, search_query, search_sort)
+			results = set_type(search_type, search_query)
+			 sorted_results = results.set_filter(search_sort)
+			 sorted_results.uniq
+		end
+
+
+		def set_type(search_type, search_query)
 			case search_type
 			when 'title'
-				run_title_search(search_query)
+				title_search(ready_search(search_query))
 			when 'ingredients'
-				run_ingredients_search(search_query)
+				ingredient_search(ready_search(search_query))
 			else
-				run_title_search(search_query)
+				all_search(ready_search(search_query))
 			end
 		end
 
-		def ready_search_string(query)
-			"%#{query.strip.downcase}%"
+		def set_filter(search_sort)
+			case search_sort
+			when 'alphabetical'
+				order(title: :asc)
+			when 'fewest ingredients'
+				order(recipe_ings_count: :asc)
+			when 'in my pantry'
+				apply_pantry_filter
+			when 'shortest preptime'
+				order(readyInMinutes: :asc)
+			else
+				order(title: :asc)
+			end
 		end
 
-		def run_title_search(query)
-			phrase_match_results = Recipe.where("lower(title) LIKE ?", ready_search_string(query))
+		def ready_search(query)
+			search_array = []
+			query.split(' ').each do |word|
+				word = word.strip.downcase
+				singular = word.singularize
+				plural = word.pluralize
+				if plural =~ /.*ies$/
+					search_array << "%#{plural.split('ies')[0]}%"
+				else
+					search_array << "%#{singular}%"
+				end
+			end
+			search_array
 		end
 
-		def run_ingredients_search(query)
-			phrase_match_results = Recipe.joins(:ingredients).where("lower(ingredients.name) LIKE ?", ready_search_string(query))
+		def all_search(query_array)
+			where_statement = Recipe.joins(:ingredients)
+			query_array.each do |word|
+				where_statement = where_statement.where("lower(name) LIKE ? OR lower(title) LIKE ?",
+				word, word)
+			end
+			where_statement
+		end
+
+		def ingredient_search(query_array)
+			where_statement = Recipe.joins(:ingredients)
+			query_array.each do |word|
+				where_statement = where_statement.where("lower(ingredients.name) LIKE ?", word)
+			end
+			where_statement
+		end
+			
+		def title_search(query_array)
+			where_statement = Recipe
+			query_array.each do |word|
+				where_statement = where_statement.where("lower(title) LIKE ?", word)
+			end
+			where_statement
+		end
+
+		def apply_pantry_filter
+			pantry = Pantry.ingredients.all
+			select { |i| (i.ingredients - pantry).count <= 3 }
 		end
 	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,9 +25,10 @@ ActiveRecord::Schema.define(version: 20160927160345) do
   end
 
   create_table "ingredients", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",                          null: false
+    t.integer  "recipe_ings_count", default: 0
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
   end
 
   create_table "list_ings", force: :cascade do |t|
@@ -99,15 +100,16 @@ ActiveRecord::Schema.define(version: 20160927160345) do
   end
 
   create_table "recipes", force: :cascade do |t|
-    t.string   "title",          null: false
+    t.string   "title",                         null: false
     t.integer  "readyInMinutes"
     t.string   "image"
     t.integer  "servings"
     t.string   "creditText"
     t.string   "type"
+    t.integer  "recipe_ings_count", default: 0
     t.integer  "user_id"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
     t.index ["user_id"], name: "index_recipes_on_user_id", using: :btree
   end
 
