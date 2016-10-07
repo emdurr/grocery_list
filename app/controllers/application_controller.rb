1da50bf75e7be1ff51f1836f27c696e5498ea5d2
Eric Durr <ericdurr@Erics-MacBook-Pro.local>
2016-10-07 08:48:14 -0600

    more hero

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :authenticate_user!
   before_filter :configure_sign_up_params, if: :devise_controller?
   protected
   	def configure_sign_up_params
